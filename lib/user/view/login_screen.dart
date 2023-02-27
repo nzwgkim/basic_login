5c49e9257d06d536134af80a5e12c97d11805059
Woody <nz.wgkim@gmail.com>
2023-02-27 19:13:49 +1300

    s1_v12, Dio로 Auth API 요청. '회원가입'에서 죽는다.

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -1,57 +1,106 @@
-import 'package:basic_login/common/const/colors.dart';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:basic_login/common/component/custom_text_form_field.dart';
+import 'package:basic_login/common/const/colors.dart';
 import 'package:basic_login/common/layout/default_layout.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-//s1_v3
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final dio = Dio();
+
+    // localhost
+    const emulatorIp = '10.0.2.2:3000';
+    const simulatorIp = '127.0.0.1:3000';
+
+    final ip = Platform.isIOS ? simulatorIp : emulatorIp;
+
     return DefaultLayout(
       child: SingleChildScrollView(
         keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
         child: SafeArea(
           top: true,
           bottom: false,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const _Title(),
-              const _SubTitle(),
-              Image.asset(
-                'asset/img/misc/logo.png',
-                width: MediaQuery.of(context).size.width / 3 * 2,
-              ),
-              CustomTextFormField(
-                hintText: '이메일을 입력해주세요.',
-                onChanged: (String value) {},
-              ),
-              CustomTextFormField(
-                hintText: '비밀번호를 입력해주세요.',
-                onChanged: (String value) {},
-                obscureText: true,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: PRIMARY_COLOR,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const _Title(),
+                const SizedBox(height: 16.0),
+                const _SubTitle(),
+                Image.asset(
+                  'asset/img/misc/logo.png',
+                  width: MediaQuery.of(context).size.width / 3 * 2,
                 ),
-                child: const Text(
-                  '로그인',
+                CustomTextFormField(
+                  hintText: '이메일을 입력해주세요.',
+                  onChanged: (String value) {},
                 ),
-              ),
-              TextButton(
-                onPressed: () {},
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.black,
+                const SizedBox(height: 16.0),
+                CustomTextFormField(
+                  hintText: '비밀번호를 입력해주세요.',
+                  onChanged: (String value) {},
+                  obscureText: true,
                 ),
-                child: const Text(
-                  '회원가입',
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () async {
+                    // ID:비밀번호
+                    const rawString = 'test@codefactory.ai:testtest';
+
+                    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+
+                    String token = stringToBase64.encode(rawString);
+
+                    final resp = await dio.post(
+                      'http://$ip/auth/login',
+                      options: Options(
+                        headers: {
+                          'authorization': 'Basic $token',
+                        },
+                      ),
+                    );
+
+                    print(resp.data);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: PRIMARY_COLOR,
+                  ),
+                  child: const Text(
+                    '로그인',
+                  ),
                 ),
-              ),
-            ],
+                TextButton(
+                  onPressed: () async {
+                    const refreshToken =
+                        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTY1Nzg1MTA3MiwiZXhwIjoxNjU3OTM3NDcyfQ.E0z7pf-nGdH5nEhGiew0SOBzM1ja69Fb1zVQ-8C4sFE';
+
+                    final resp = await dio.post(
+                      'http://$ip/auth/token',
+                      options: Options(
+                        headers: {
+                          'authorization': 'Bearer $refreshToken',
+                        },
+                      ),
+                    );
+
+                    print(resp.data);
+                  },
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                  ),
+                  child: const Text(
+                    '회원가입',
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
